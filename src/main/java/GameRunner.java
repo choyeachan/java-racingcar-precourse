0e53c0a823be2f16e7e54de851adbdc1e46c8792
조예찬 <choyuichan@gmail.com>
2021-05-04 22:34:24 +0900

    feat: Scanner 객체 추가 및 게임 진행 메서드 추가
    - RacingGame.run : Scanner 로 System.in 으로 들어온 입력값을 입력 받고 게임을 진행하는 메서드

--- src/main/java/GameRunner.java
+++ src/main/java/GameRunner.java
@@ -2,9 +2,7 @@ public class GameRunner {
 
 	public static void main (String[] args){
 		RacingGame game = new RacingGame();
-		game.readyForRacing("1번,2번,3번", ",");
-		game.tryCount(5);
-		game.race();
+		game.run();
 	}
 
 }
--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class RacingGame{
 
@@ -8,9 +9,22 @@ public class RacingGame{
 	public int tryCount=0;
 
 	private Random r;
+	private Scanner scan;
 
 	RacingGame(){
 		r = new Random();
+		scan = new Scanner(System.in);
+	}
+
+	public void run(){
+		System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+		String carNames = scan.next();
+		readyForRacing(carNames, ",");
+		System.out.println("시도할 회수는 몇회인가요?");
+		String tryCount = scan.next();
+		tryCount(Integer.parseInt(tryCount));
+		race();
+		outputResult(cars);
 	}
 
 	public void readyForRacing(String carsString,String seperator){
@@ -63,7 +77,7 @@ public class RacingGame{
 		for(Car car: cars.pop()){
 			winnerPosition = getWinnersPosition(winnerPosition, car);
 		}
-		return getWinners(winnerPosition, cars);
+		return getWinners(winnerPosition, cars)+"가 최종 우승했습니다.";
 	}
 
 	public int getWinnersPosition(int winnerPosition, Car car){
